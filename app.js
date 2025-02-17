78a3cc1b8183d3a112bd74c3fe2e6b66272c02db
Maryam Shabbir <maryamshabbir229@gmail.com>
2025-02-17 17:27:19 +0500

    practice Javascpt

---
+++ app.js
@@ -0,0 +1,479 @@
+// Chapter 5 (Math Expressions)
+
+// Q1
+
+// var a = 3;
+// var b = 5;
+// var c = a+b;
+// document.write("Sum of ", a , " and " , b , " is " , c)
+
+// Q2
+
+// var a = 3;
+// var b = 5;
+// var c = a-b;
+// document.write("Subtraction of ", a , " and " , b , " is " , c)
+
+
+
+// var a = 3;
+// var b = 5;
+// var c = a*b;
+// document.write("Multiplication of ", a , " and " , b , " is " , c)
+
+
+
+// var a = 3;
+// var b = 5;
+// var c = a/b;
+// document.write("Division of ", a , " and " , b , " is " , c)
+
+
+
+// var a = 3;
+// var b = 5;
+// var c = a%b;
+// document.write("Modulus of ", a , " and " , b , " is " , c)
+
+
+// Q3
+
+// var a;
+// document.write( "Value after variable declaration is " , a);
+// a = 5;
+// document.write(" <br> Initial value " , a);
+// a = ++a;
+// document.write("<br> Value after increment is " , a);
+// a = a + 7;
+// document.write("<br> Value after addition is " , a);
+// a = --a;
+// document.write("<br> Value after decrement is " , a);
+// a = a / 3;
+// document.write("<br>The reminder is " , a);
+
+
+// Q4
+
+
+// var ticketPrice = 600;
+// var buy = 5;
+// var total = 600*5;
+// document.write("Total cost of buy " , buy , " tickets to a movie is " , total , "Pkr")
+
+
+// Q5
+
+
+// document.write("<h3>Table of 5 </h3>");
+// var a = 5;
+// var b = a*2
+// var c = a*3;
+// var d = a*4;
+// var e = a*5;
+// var f = a*6;
+// var g = a*7;
+// var h = a*8;
+// var i = a*9;
+// var j = a*10;
+// document.write("5x1 = ", a , "<br> 5x2 = ", b , "<br>5x3 = ", c , "<br>5x4 = ", d , "<br>5x5 = ", e , "<br>5x6 = ", f , "<br>5x7 = ", g , "<br>5x8 = ", h , "<br>5x9 = ", i , "<br>5x10 = ", j )
+
+
+// Q6
+
+// var T1 = 25;
+// var F = ( T1 * 9/5) + 32;
+// document.write(T1, "C" , " is " , F , "F <br>");
+
+// var T2 = 70;
+// var C = ( T2 - 32) *5/9;
+// document.write( T2, "F" , " is " , C , "C");
+
+
+
+// Q7
+
+
+
+// var item1 = 1000;
+// var item2 = 2000;
+// var order1 = 5;
+// var order2 = 2;
+// var shippingCharges = 200;
+// document.write("Price of item 1 is ", item1 , "<br> Quantity of item 1 is ", order1 , "<br>Price of Item 2 is " , item2 ,"<br>Quantity of Item 2 is " , order2 , "<br>Shipping charges are " , shippingCharges);
+
+// document.write( "<br> total cost of your order is " , (item1 * order1) + (item2 * order2) + shippingCharges)
+
+
+
+// Q8
+
+
+// var totalMarks = 850;
+// var obtainedMarks = 785;
+// var percentage = (obtainedMarks/totalMarks) *100;
+// document.write("Total Marks : " , totalMarks , "<br>Obtained Marks : " , obtainedMarks , "<br>Percentage : " , percentage)
+
+
+
+// Q9
+
+
+
+// var US = 10;
+// var  riyals = 25;
+// document.write("Total currency in PKR " , (10*104.80) + (25*28));
+
+
+
+// Q10
+
+
+
+
+// var a = 5;
+// document.write(((a+5)* 10) /2  )
+
+
+
+// Q11
+
+
+// var currentYear = 2025;
+// var birthYear = 2005;
+// document.write("Current Year : " , currentYear , "<br>Birth Year : " , birthYear , "<br>Your age is " , currentYear - birthYear)
+
+
+
+// Q12
+
+// var radius = 20;
+// var circumference = 2*3.141*radius;
+// var area = 3.141*radius*2
+// document.write( "The radius of circle is ", radius , "<br>The circumference of circle is  " , circumference , "<br> The area is " , area )
+
+
+
+// Q13
+
+
+// var snack =" chocolate chip";
+// var currentAge = 20;
+// var maxAge = 70;
+// var perDay = 5 ; 
+// document.write("Favorite Snack " , snack , "<br>Current age is " , currentAge , "<br>Estimated Maximum Age  " , maxAge , "<br>Amount of snacks per day " , perDay , "<br> You Will need " ,  (maxAge*perDay) - (currentAge*perDay) , " chocolate chip to last to untill the ripe old age of " , maxAge  ) 
+
+
+
+
+// Chapter 6-9 (Math Expressions)
+
+
+
+// Q1
+
+
+
+// var a = 10;
+// document.write("The value of a is " , a)
+// var b = a + ++a;
+// document.write("<br>The value of ++a is " , 11);
+// document.write("<br>The value of a is " , a);
+//  var c = a + ++a
+// document.write("<br>The value of ++a is " , 11);
+// document.write("<br>The value of a is " , a);
+//  var d = a + --a
+// document.write("<br>The value of --a is " , 11);
+// document.write("<br>The value of a is " , a);
+// var e = a + a--;
+// document.write("<br>The value of --a is " , 11);
+// document.write("<br>The value of a is " , a);
+
+
+
+// Q2
+
+
+// var a = 2, b = 1;
+// var result = --a - --b + ++b + b--;
+// document.write(result)
+
+// --a; 1
+// --a - --b; = 1 - 0 = 1
+// --a - --b + ++b;= 1 - 0 + 1 = 2
+// --a - --b + ++b + b--; = 1 - 0 + 1 + 1 = 3
+
+
+
+// Q3
+
+
+// prompt("Welocome to our Website")
+
+
+// Q5
+
+
+// var inputValue = +prompt("Enter value ")
+// var Value = (inputValue * 5)
+// console.log(Value)
+
+
+// Chapter 6 (Math expressions II)
+
+
+
+// Q1
+
+
+
+// var x = x++
+
+
+// Q2
+
+
+// var x = 100;
+// var x = x + --x;
+
+
+
+// Q3
+
+
+// The value of y will be = 50;
+
+
+
+// Q4
+
+// The value of y will be = 49
+
+
+// Q5
+// var num = 10;
+// var num = --num;
+// var newnum = num;
+// alert(newnum);
+
+
+// Q6
+
+
+// var num = 5;
+// var newnum = num++;
+// alert (newnum);
+// alert (num)
+
+
+// Q7
+
+
+
+// var a = 3;
+// var a = ++a;
+// alert(a)
+
+
+
+// Chapter 7 (Math expression III)
+
+
+
+// Q1
+
+
+// var calculatedNum = 2 + (2 * 6); 
+// alert(calculatedNum);
+
+// Q2
+
+
+//  var calculatedNum = (2 + 2) * 6; 
+//  alert(calculatedNum);
+
+
+
+// Q3
+
+
+	// var calculatedNum = (2 + 2) * (4 + 2); 
+    // alert(calculatedNum);
+
+
+
+    // Q4
+
+
+
+
+   	// var calculatedNum = ((2 + 2) * 4) + 2; 
+    // alert(calculatedNum);
+
+
+
+    // Q5
+
+
+// var a = 2 + 2 * 4 + 10;
+// document.write("the result is " , a );
+
+
+
+// Q6
+
+// var a = 2 + 2 * 4 + 10;
+// document.write("the result is " , a );
+
+
+
+
+// Q7
+
+// var a = 4 / 2 * 4 ;
+// document.write("the result is " , a );
+
+
+
+// chpater 8 (Concatenating )
+
+
+// Q1
+
+
+// var num = "2" + "2"; 
+// alert(num)
+
+
+
+// Q2
+
+
+
+// var	message = ("Hello," + "Dolly"); 
+// alert(message)
+
+
+
+// Q3
+
+
+// alert("33" + 3);  
+
+
+
+// Q4
+
+
+
+// var a = "Pakistan" + " Zindabad"
+// alert(a)
+
+
+
+// Q5
+
+
+// var a = "Maryam" + 22;
+// alert(a)
+
+
+// Q6
+
+
+
+// var a = "Maryam";
+// var b = "Shabbir";
+// var c = a + b;
+// alert(c)
+
+
+
+// Chapter 9 (Prompts)
+
+
+
+// Q1
+
+
+// prompt("Enter first Name")
+
+
+
+// Q2
+
+
+
+// prompt("Country" , "China")
+
+
+
+// Q3
+
+
+
+// var yourName = prompt("Enter Your Name"); 
+
+
+
+// Q4
+
+
+// prompt("Enter your religion" , "Islam")
+
+
+// Q5
+
+
+// var a = "Your Name"
+// var b = "Maryam"
+//  var userResponse = prompt(a , b) ;
+
+
+
+// Q6
+
+
+//  var qualification = prompt("Qualification" , "Intermediate")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
